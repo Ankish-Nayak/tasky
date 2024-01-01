faf0ff3e3f7f0646140b3cb36f1ee4c50e5d4c86
ankish2000nayak@gmail.com <ankish2000nayak@gmail.com>
2024-01-01 17:57:32 +0530

    connected To DB

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,2 +1,26 @@
+import { connect } from "mongoose";
 export * from "./tasks";
 export * from "./users";
+
+export const run = async (): Promise<boolean> => {
+  return new Promise(async (res, rej) => {
+    try {
+      const url = process.env.DATABASE_URL;
+      const dbName = process.env.DATABASE_NAME;
+      if (typeof url === "undefined") {
+        throw new Error("data base url not found");
+      }
+      if (typeof dbName === "undefined") {
+        throw new Error("database name not found");
+      }
+      console.log(url);
+      await connect(url, {
+        dbName: dbName,
+      });
+      res(true);
+    } catch (e) {
+      console.log(e);
+      rej(false);
+    }
+  });
+};
